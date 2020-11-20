5c076a8fb9617b09616d802e981145ddf3b2d73b
solidponcho <42821325+solidponcho@users.noreply.github.com>
2020-11-20 17:57:37 -0600

    solved all methods

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
 namespace ChallengeSets
 {
     public class ChallengeSet01
@@ -9,29 +11,33 @@ namespace ChallengeSets
         }    
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend -= subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 += number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 -= number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 *= factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            return $"Hello!";
+                
         }
-
+        
+        
+            
+        
         public string GetHey()
         {
             return "HEY!";
